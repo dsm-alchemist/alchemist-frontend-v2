18048650a733b4e09d69cc952808f7cbda40ae94
seockhun <hun00719@gmail.com>
2021-11-26 09:36:04 +0900

    [CREATE] ColorMap
    main color export

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,1 +1,11 @@
-export const COLOR = "red";
+export const COLOR = {
+    blackColor: "#000000",
+    whiteColor: "#FFFFFF",
+
+    blue100: "#1DA0FF",
+    blue200: "#7F92FC",
+
+    red100: "#FF4545",
+
+
+};
