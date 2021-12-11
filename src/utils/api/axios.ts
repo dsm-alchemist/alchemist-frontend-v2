3312adfdee5781b9e45f04574084d5ea19fecd35
seockhun <hun00719@gmail.com>
2021-12-11 13:48:42 +0900

    [UPDATE] refresh_token

--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -9,6 +9,7 @@ interface AxiosProps {
 
 const BASE_URL = "http://52.79.148.224:8080";
 export const ACCESS_TOKEN = 'alchemist_access_token';
+export const REFRESH_TOKEN = 'alchemist_refresh_token';
 
 export const requestWithOutAccessToken = ({ method, url, headers, data }: AxiosProps) => {
     return axios({
