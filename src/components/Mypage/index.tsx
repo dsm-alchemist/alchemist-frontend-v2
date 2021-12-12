eee483a0e7427d06b92d94793cec758592b81192
seockhun <hun00719@gmail.com>
2021-12-12 22:16:24 +0900

    [CREATE] myPage

---
+++ src/components/Mypage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+const UserList = () => {
+    return (
+        <S.Wrapper>
+            
+        </S.Wrapper>
+    )
+}
+
+export default UserList;
